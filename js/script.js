007558a234394940a80add9fd4deded4382c856f
Kamronbek Olimovgit config --user.namegit config user.namegit config --global user.name Kamronbek <kamronbekolimov116@gmail.com>
2024-08-10 23:23:23 +0500

    restart version

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-let hour = document.getElementById("hour")
-let min = document.getElementById("min")
-let sec = document.getElementById("sec")
-
+let hour = document.getElementById(`hour`)
+let min = document.getElementById(`min`)
+let sec = document.getElementById(`sec`)
 const time = () => {
     let date = new Date()
     let hh = date.getHours()
     let mm = date.getMinutes()
     let ss = date.getSeconds()
-
     let hRotation = 30*hh + mm/2
-    let mRotation = 6*mm
-    let sRotation = 6*ss
-
+    let mRotation = mm*6
+    let sRotation = ss*6
     hour.style.transform = `rotate(${hRotation}deg)`
     min.style.transform = `rotate(${mRotation}deg)`
     sec.style.transform = `rotate(${sRotation}deg)`
